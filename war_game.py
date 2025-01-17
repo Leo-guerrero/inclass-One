779a4ad391deee7f810bf306c1b3bb3460eec5fb
Leo-guerrero <sja.leo.guerrero@gmail.com>
2025-01-16 19:18:39 -0600

    Update war_game.py
    changed suit to equal suit = ["Pork", "Donut", "Steak", "Meat"]

--- war_game.py
+++ war_game.py
@@ -7,7 +7,8 @@
 
 # Global variables used to create a new deck
 face = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
-suit = ["Clubs", "Diamonds", "Hearts", "Spades"]
+# Change from Branch Five AND Six
+suit = ["Pork", "Donut", "Steak", "Meat"]
 
 import random
 
